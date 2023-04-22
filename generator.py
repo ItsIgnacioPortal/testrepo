441b5dfa69c910f6532b7fbe3cc7c0e3a494c3a9
Ignacio J. Perez Portal <5990@protonmail.com>
2023-04-22 14:06:19 -0300

    feat: Fixed format and uploaded generator script

---
+++ generator.py
@@ -0,0 +1,14 @@
+limit = 300
+
+print("[myheader]")
+print("mykey=[")
+for i in range(1,limit):
+	print("\t\"", end="")
+	for i in range(0,i):
+		print("a", end="")
+
+	if i != limit-2:
+		print("\",")
+	else:
+		print("\"")
+		print("]")
